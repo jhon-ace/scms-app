d4779c216313b625ca4e50498ac4db7a9ed9c04e
jhon-ace <justcodingph.97@gmail.com>
2024-05-28 15:57:41 +0800

    2nd commit

--- resources/views/user-pages/index.blade.php
+++ resources/views/user-pages/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Student Classroom Management System</title>
+        <title>{{ config('app.name')}}</title>
 
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <!-- If you have a larger or different format favicon, you can include it as well -->
@@ -12,79 +12,48 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
+        
 
 
         @vite('resources/css/app.css')
     </head>
-<body class="{{ session('dark_mode') ? 'bg-dark' : '' }}">
-    <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+<body class="bg-200">
+    <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center ">
         <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
             <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
                 <div>
                     <img src="{{asset('assets/images/logo.png')}}"
-                        class="w-mx-auto" />
+                        class="w-mx-auto"/>
                 </div>
                 <div class="flex flex-col items-center">
                     <div class="w-full flex-1">
-                        <div class="flex flex-col items-center">
-                            
-                            <button
-                                class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-green-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
-                                <div class="bg-white p-2 rounded-full">
-                                    <svg class="w-4" viewBox="0 0 533.5 544.3">
-                                        <path
-                                            d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
-                                            fill="#4285f4" />
-                                        <path
-                                            d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
-                                            fill="#34a853" />
-                                        <path
-                                            d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
-                                            fill="#fbbc04" />
-                                        <path
-                                            d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
-                                            fill="#ea4335" />
-                                    </svg>
-                                </div>
-                               
-                                <span class="ml-4">
-                                    Sign In with Google
-                                </span>
-                            </button>
-
+                        <div class="flex mb-5 flex-col items-center -mt-5 font-sans text-md">
+                        {{ config('app.name')}}
                         </div>
-
-                        <div class="my-12 border-b text-center">
-                            <div
-                                class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                                Or sign In SCMS App Credentials
-                            </div>
-                        </div>
-
                         <div class="mx-auto max-w-xs">
                             <input
-                                class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                                class="w-full px-5 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                                 type="email" placeholder="Email" />
                             <input
-                                class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                class="w-full px-5 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                                 type="password" placeholder="Password" />
                             <button
-                                class="mt-5 tracking-wide font-semibold bg-green-400 text-white-500 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                                <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                                class="bg-600 border  border-black mt-10 tracking-wide font-semibold  text-white-500 w-full py-4 rounded-lg hover:bg-green-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                <svg class="w-6 h-6 -ml-2" fill="none" stroke="white" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
                                     <circle cx="8.5" cy="7" r="4" />
                                     <path d="M20 8v6M23 11h-6" />
                                 </svg>
-                                <span class="ml-">
+                                <span class="ml- text-white">
                                     Sign In
                                 </span>
                             </button>
                             <p class="mt-4 text-sm text-gray-600 text-center">
                                 Don't have an account? <a
                                         href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
+                                        class="rounded-md text-black ">
                                         Log in
                                     </a>
                                
@@ -93,11 +62,36 @@
                     </div>
                 </div>
             </div>
-            <div class="flex-1 bg-green-100 text-center hidden lg:flex">
-                <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                style="background-image: url('{{ asset('assets/images/bg.png') }}');">
+            <div class="flex-1 bg-600 text-center hidden lg:flex rounded-br-lg rounded-tr-lg">
+                <div class="m-12 xl:m-20 w-full bg-contain text-white  font-sans font-normal bg-center bg-no-repeat">
+                    <div class="bg-contain font-normal fonts-sans text-2xl font-agbalumo">
+                        Student Classroom Management System
+                    </div>
+                    <div class="mx-auto text-justify mt-10 tracking-wider">
+                        <p>
+                            The Student Classroom Management System (SCMS) is an innovative software solution designed to streamline and enhance the educational experience for
+                            both students and educators. This comprehensive platform integrates various functionalities to manage classroom activities, monitor student progress, 
+                            and facilitate effective communication.
+                        </p><br>
+                        <p>
+                        SCMS offers a centralized dashboard where educators can easily create, distribute, and manage assignments. Teachers can track student submissions, grade
+                        assignments, and provide feedback promptly. The system also supports automated grading for multiple-choice and other objective-type questions, saving 
+                        valuable time for educators.
+                        </p><br>
+
+                        <p>
+                        Additionally, SCMS includes analytics tools that provide insights into student performance at both individual and class levels. 
+                        These insights help educators to tailor their teaching strategies to meet the specific needs of their students, thereby improving learning outcomes.
+                        </p><br>
+
+                        <p>
+                        Overall, the Student Classroom Management System is a valuable asset for modern educational institutions, enhancing efficiency, fostering better 
+                        communication, and supporting data-driven decision-making.
+                        </p>
+                    
+                    </div>
                 </div>
-            </div>
+            
         </div>
     </div>
     </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,32 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans, 'sans-serif'],
+                'titan-one': ['"Titan One"', 'sans-serif'],
+                'agbalumo': ['"Agbalumo"', 'system-ui'],
+
             },
         },
+        colors: {
+
+            'white': '#ffffff',
+            'purple': '#3f3cbb',
+            'midnight': '#3a4750',
+            'metal': '#565584',
+            'tahiti': '#3ab7bf',
+            'silver': '#ecebff',
+            'bubble-gum': '#ff77e9',
+            'bermuda': '#78dcca',
+            '600' :'#E8626D',
+            '200': '#CDCBCC',
+          },
     },
+    screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
 
     plugins: [forms],
 };
